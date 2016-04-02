7d4d628a7ed9bb52ba0a0e6a90ed229a5e8ded4d
AnnaGulstine <annagulstine@gmail.com>
2016-04-02 18:16:01 -0500

    Initial commit

---
+++ calc_1.rb
@@ -0,0 +1,30 @@
+class SimpleCalculator
+  def add(first_number, second_number)
+    first_number + second_number
+  end
+
+  def subtract(first_number, second_number)
+    first_number - second_number
+  end
+
+  def multiply(first_number, second_number)
+    first_number * second_number
+  end
+
+  def divide(first_number, second_number)
+    first_number / second_number
+  end
+end
+
+class FancyCalculator < SimpleCalculator
+  def square_root(number)
+    Math.sqrt(number)
+  end
+end
+
+calc = FancyCalculator.new
+puts calc.add(4, 3)
+puts calc.subtract(4, 3)
+puts calc.multiply(4, 3)
+puts calc.divide(6, 3)
+puts calc.square_root(4)
